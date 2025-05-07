c57eecc5a4d3a62bae7290588cebcc929c3b7de2
Suchir Srivastava <suchir0311@gmail.com>
2025-05-06 20:59:05 -0700

    added stop tracking and stop tracking buttons to extension popup. Verified buttons using console.log statements. Buttons send chrome.runtime messages

--- src/popup.ts
+++ src/popup.ts
@@ -4,10 +4,24 @@ console.log("Popup loaded");
 document.addEventListener("DOMContentLoaded", function () {
   // waits for HTML document to be fully loaded and parsed
   const startTrackingBtn = document.getElementById("startTrackingButton");
+  const stopTrackingBtn = document.getElementById("stopTrackingButton");
+
+  console.log("Start button:", startTrackingBtn);
+  console.log("Stop button:", stopTrackingBtn);
+
   if (startTrackingBtn) {
     // ensure that startTrackingBtn exists before trying to handle a click event
     startTrackingBtn.addEventListener("click", () => {
+      console.log("Start button clicked");
       chrome.runtime.sendMessage({ type: "START_TRACKING" });
+    });
+  }
+
+  if (stopTrackingBtn) {
+    // ensure that stopTrackingBtn exists before trying to handle a click event
+    stopTrackingBtn.addEventListener("click", () => {
+      console.log("Stop button clicked");
+      chrome.runtime.sendMessage({ type: "STOP_TRACKING" });
     });
   }
 });
