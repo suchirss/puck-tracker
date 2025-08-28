45959f7c5124534b9ca13320eb6e805c28fead2f
Suchir Srivastava <suchir0311@gmail.com>
2025-08-27 21:59:44 -0600

    added canvas overlay over video clip. Used openCV to draw circle on canvas

--- opencv_isolated_tracker/tracker.js
+++ opencv_isolated_tracker/tracker.js
@@ -14,5 +14,29 @@ function onOpenCvReady() {
   playVideoButton.onclick = () => {
     video.play();
     video.loop = true;
+
+    // vars for reused video dimensions
+    const videoWidth = video.videoWidth;
+    const videoHeight = video.videoHeight;
+
+    // create openCV mat using video dimensions
+    const mat = new cv.Mat(videoHeight, videoWidth, cv.CV_8UC4);
+
+    // set up vars to use cv.circles
+    const videoCenter = new cv.Point(videoWidth / 2, videoHeight / 2);
+    const radius = 50;
+    const colour = new cv.Scalar(255, 0, 0, 255);
+    const thickness = 3;
+
+    cv.circle(mat, videoCenter, radius, colour, thickness);
+
+    // render the openCV Mat on the canvas
+    const canvas = document.getElementById("outputCanvas");
+    canvas.width = videoWidth;
+    canvas.height = videoHeight;
+    cv.imshow(canvas, mat);
+
+    // remove Mat
+    mat.delete();
   };
 }
